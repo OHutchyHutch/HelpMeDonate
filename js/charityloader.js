d2cfdf7263275d00e11b1037ecc5fc039c22e590
OHutchyHutch <ohutchins@comcast.net>
2020-06-10 00:27:59 -0400

    Website V 0.19
    Continuing test from V 0.18

--- js/charityloader.js
+++ js/charityloader.js
@@ -1,15 +1,14 @@
 $( "#CurrentEventsButton" ).click(function() {
     $.getJSON('charities/charities.json', function(data) {
         $.each(data.currentevents, function(key, val) {
-            alert(val.charityName);
-            alert(val.charityGoal);
-            alert(val.charityImage);
             $( "#CurrentEventsCharities" ).append( "<div id=\""+val.charityName+"\"></div>");
             alert("TEST:"+val.charityName);
             $( "#"+val.charityName ).append( "<div class=\"card mb-3 my-5\" style=\"max-width: 540px;\" id=\""+ val.charityName +"CARD\"></div>");
             alert("TEST:"+val.charityName);
             $( "#"+val.charityName+"CARD" ).append("<div class=\"row no-gutters\" id=\""+ val.CharityName +"ROW\"></div>");
+            alert("TEST:"+val.charityName);
             $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-4\" id=\""+ val.CharityName +"IMAGEHOLD\"></div>");
+            alert("TEST:"+val.charityName);
             $( "#"+val.charityName+"IMAGEHOLD" ).append("<img src=\""+ val.charityImage +"\" class=\"card-img\"");
          })
     });
