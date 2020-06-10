c0766c4f8d06e8334902753854e7b92745e0972f
OHutchyHutch <ohutchins@comcast.net>
2020-06-09 23:36:37 -0400

    Website V 0.11
    Cleanup and started working on a json file and charityloader.js to read through the json file to know the charities.

--- js/charityloader.js
+++ js/charityloader.js
@@ -1,6 +1,28 @@
 $( "#CurrentEventsButton" ).click(function() {
-    jQuery.get('charities/test.txt', function(data) {
-        alert(data);
-        //$('#text').text(data);
-     });
+    $.getJSON('testData.json', function(data) {
+        $.each(data.currentevents, function(key, val) {
+            alert(val.charityName);
+            alert(val.charityGoal);
+            alert(val.charityImage);
+         })
+    });
   });
+
+
+  /*
+  <div id="CHARITY TITLE">
+    <div class="card mb-3 my-5" style="max-width: 540px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="CHARITY IMAGE" class="card-img">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">CHARITY TITLE</h5>
+                      <p class="card-text">CHARITY DESCRIPTION</p>
+                    </div>
+                  </div>
+                </div>
+        </div>
+    </div>
+  */
