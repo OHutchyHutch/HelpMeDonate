6af58f2865f3b23c5eac646bc6838445a7e260c1
OHutchyHutch <ohutchins@comcast.net>
2020-06-10 00:49:36 -0400

    Website V 0.24
    Fixed an error where some items weren't appearing

--- js/charityloader.js
+++ js/charityloader.js
@@ -6,7 +6,9 @@ $( "#CurrentEventsButton" ).click(function() {
             $( "#"+val.charityName+"CARD" ).append("<div class=\"row no-gutters\" id=\""+ val.charityName +"ROW\"></div>");
             $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-4\" id=\""+ val.charityName +"IMAGEHOLD\"></div>");
             $( "#"+val.charityName+"IMAGEHOLD" ).append("<img src=\""+ val.charityImage +"\" class=\"card-img\" alt=\"IMAGE FAILED TO LOAD\">");
-            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-8\"></div>").append("<div class=\"card-body\"></div>").append("<h5 class=\"card-title>"+val.charityName+"</h5>");
+            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-8\" id=\""+val.charityName+"CARDCOL\"></div>")
+            $( "#"+val.charityName+"CARDCOL" ).append("<div class=\"card-body\" id=\""+val.charityName+"CARDBODY\"></div>")
+            $( "#"+val.charityName+"CARDBODY" ).append("<h5 class=\"card-title>"+val.charityName+"</h5>").append("<p> class=\"card-text\">"+val.charityGoal+"</p>");
          })
     });
   });
