134d89d9e705eb9b5a3dff7de3c5de461764f433
OHutchyHutch <ohutchins@comcast.net>
2020-06-10 14:55:54 -0400

    Website V 0.42
    Continuing panel testing

--- js/charityloader.js
+++ js/charityloader.js
@@ -19,3 +19,26 @@ $( "#CurrentEventsButton" ).one('click', function () {
     });
     console.log("Goodbye!");
   });
+
+  $( "#TestButton" ).one('click', function () {
+    console.log("Getting json");
+    $.getJSON('charities/charities.json', function(data) {
+        console.log("Reading Json");
+        $.each(data.currentevents, function(key, val) {
+            charityName = val.charityName; 
+            charityNameSpaces = charityName.replace('-', ' '); 
+            $( "#TestButton" ).append( "<div id=\""+val.charityName+"\"style=\"width:40%; display:inline-block; margin:4.5%;\"></div>");
+            $( "#"+val.charityName ).append( "<div class=\"card mb-3 my-5\" style=\"max-width: 100%; max-height: 100%;\" id=\""+ val.charityName +"CARD\"></div>");
+            $( "#"+val.charityName+"CARD" ).append("<div class=\"row no-gutters\" id=\""+ val.charityName +"ROW\"></div>");
+            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-4\" id=\""+ val.charityName +"IMAGEHOLD\"></div>");
+            $( "#"+val.charityName+"IMAGEHOLD" ).append("<img src=\""+ val.charityImage +"\" class=\"card-img\" alt=\"IMAGE FAILED TO LOAD\">");
+            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-8\" id=\""+val.charityName+"CARDCOL\"></div>")
+            $( "#"+val.charityName+"CARDCOL" ).append("<div class=\"card-body\" id=\""+val.charityName+"CARDBODY\"></div>")
+            $( "#"+val.charityName+"CARDBODY" ).append("<h5 class=\"card-title\">"+charityNameSpaces+"</h5>").append("<p class=\"card-text\">"+val.charityGoal+"</p>");
+            console.log("Initialized " + val.charityName);
+         })
+         console.log("Everything should be done now!");
+    });
+    console.log("Goodbye!");
+  });
+
