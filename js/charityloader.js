8a67d89965d8002c5e34f82bcee62a9e174337bd
OHutchyHutch <ohutchins@comcast.net>
2020-06-09 23:46:31 -0400

    Website V 0.14
    Fixed reference error... again

--- js/charityloader.js
+++ js/charityloader.js
@@ -1,6 +1,6 @@
 $( "#CurrentEventsButton" ).click(function() {
     alert("JSON TEST 1");
-    $.getJSON('charities/testData.json', function(data) {
+    $.getJSON('charities/charities.json', function(data) {
         alert("JSON TEST 2");
         $.each(data.currentevents, function(key, val) {
             alert("JSON TEST 3");
