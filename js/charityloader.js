85c509f0f063c4c65ae38451a508fb563d41f389
OHutchyHutch <ohutchins@comcast.net>
2020-06-10 18:19:09 -0400

    Website V 0.52
    Added some more permanent naming conventions to charities.html. Made charityloader.js more readable.

--- js/charityloader.js
+++ js/charityloader.js
@@ -1,44 +1,34 @@
 $( "#CurrentEventsButton" ).one('click', function () {
-    console.log("Getting json");
     $.getJSON('charities/charities.json', function(data) {
-        console.log("Reading Json");
         $.each(data.currentevents, function(key, val) {
             charityName = val.charityName; 
             charityNameSpaces = charityName.replace('-', ' '); 
-            $( "#CurrentEventsCharities" ).append( "<div id=\""+val.charityName+"\"style=\"width:40%; display:inline-block; margin:4.5%;\"></div>");
-            $( "#"+val.charityName ).append( "<div class=\"card mb-3 my-5\" style=\"max-width: 100%; max-height: 100%;\" id=\""+ val.charityName +"CARD\"></div>");
-            $( "#"+val.charityName+"CARD" ).append("<div class=\"row no-gutters\" id=\""+ val.charityName +"ROW\"></div>");
-            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-4\" id=\""+ val.charityName +"IMAGEHOLD\"></div>");
-            $( "#"+val.charityName+"IMAGEHOLD" ).append("<img src=\""+ val.charityImage +"\" class=\"card-img\" alt=\"IMAGE FAILED TO LOAD\">");
-            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-8\" id=\""+val.charityName+"CARDCOL\"></div>")
-            $( "#"+val.charityName+"CARDCOL" ).append("<div class=\"card-body\" id=\""+val.charityName+"CARDBODY\"></div>")
-            $( "#"+val.charityName+"CARDBODY" ).append("<h5 class=\"card-title\">"+charityNameSpaces+"</h5>").append("<p class=\"card-text\">"+val.charityGoal+"</p>");
-            console.log("Initialized " + val.charityName);
+            $( "#CurrentEventsCharities" ).append( '<div id="'+val.charityName+'"style="width:40%; display:inline-block; margin:4.5%;"></div>');
+            $( "#"+val.charityName ).append( '<div class="card mb-3 my-5" style="max-width: 100%; max-height: 100%;" id="'+ val.charityName +'CARD"></div>');
+            $( "#"+val.charityName+"CARD" ).append('<div class="row no-gutters" id="'+ val.charityName +'ROW"></div>');
+            $( "#"+val.charityName+"ROW" ).append('<div class="col-md-4" id="'+ val.charityName +'IMAGEHOLD"></div>');
+            $( "#"+val.charityName+"IMAGEHOLD" ).append('<img src="'+ val.charityImage +'" class="card-img" alt="IMAGE FAILED TO LOAD">');
+            $( "#"+val.charityName+"ROW" ).append('<div class="col-md-8" id="'+val.charityName+'CARDCOL"></div>')
+            $( "#"+val.charityName+"CARDCOL" ).append('<div class="card-body" id="'+val.charityName+'CARDBODY"></div>')
+            $( "#"+val.charityName+"CARDBODY" ).append('<h5 class="card-title">'+charityNameSpaces+'</h5>').append('<p class="card-text">'+val.charityGoal+'</p>');
          })
-         console.log("Everything should be done now!");
     });
-    console.log("Goodbye!");
   });
-
-$( "#TestButtonButton" ).one('click', function () {
-    console.log("Getting json");
+  $( "#EnvironmentalButton" ).one('click', function () {
     $.getJSON('charities/charities.json', function(data) {
-        console.log("Reading Json");
         $.each(data.environmental, function(key, val) {
             charityName = val.charityName; 
             charityNameSpaces = charityName.replace('-', ' '); 
-            $( "#TestEnvironment" ).append( "<div id=\""+val.charityName+"\"style=\"width:40%; display:inline-block; margin:4.5%;\"></div>");
-            $( "#"+val.charityName ).append( "<div class=\"card mb-3 my-5\" style=\"max-width: 100%; max-height: 100%;\" id=\""+ val.charityName +"CARD\"></div>");
-            $( "#"+val.charityName+"CARD" ).append("<div class=\"row no-gutters\" id=\""+ val.charityName +"ROW\"></div>");
-            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-4\" id=\""+ val.charityName +"IMAGEHOLD\"></div>");
-            $( "#"+val.charityName+"IMAGEHOLD" ).append("<img src=\""+ val.charityImage +"\" class=\"card-img\" alt=\"IMAGE FAILED TO LOAD\">");
-            $( "#"+val.charityName+"ROW" ).append("<div class=\"col-md-8\" id=\""+val.charityName+"CARDCOL\"></div>")
-            $( "#"+val.charityName+"CARDCOL" ).append("<div class=\"card-body\" id=\""+val.charityName+"CARDBODY\"></div>")
-            $( "#"+val.charityName+"CARDBODY" ).append("<h5 class=\"card-title\">"+charityNameSpaces+"</h5>").append("<p class=\"card-text\">"+val.charityGoal+"</p>");
-            console.log("Initialized " + val.charityName);
+            $( "#EnviromentalCharities" ).append( '<div id="'+val.charityName+'"style="width:40%; display:inline-block; margin:4.5%;"></div>');
+            $( "#"+val.charityName ).append( '<div class="card mb-3 my-5" style="max-width: 100%; max-height: 100%;" id="'+ val.charityName +'CARD"></div>');
+            $( "#"+val.charityName+"CARD" ).append('<div class="row no-gutters" id="'+ val.charityName +'ROW"></div>');
+            $( "#"+val.charityName+"ROW" ).append('<div class="col-md-4" id="'+ val.charityName +'IMAGEHOLD"></div>');
+            $( "#"+val.charityName+"IMAGEHOLD" ).append('<img src="'+ val.charityImage +'" class="card-img" alt="IMAGE FAILED TO LOAD">');
+            $( "#"+val.charityName+"ROW" ).append('<div class="col-md-8" id="'+val.charityName+'CARDCOL"></div>')
+            $( "#"+val.charityName+"CARDCOL" ).append('<div class="card-body" id="'+val.charityName+'CARDBODY"></div>')
+            $( "#"+val.charityName+"CARDBODY" ).append('<h5 class="card-title">'+charityNameSpaces+'</h5>').append('<p class="card-text">'+val.charityGoal+'</p>');
          })
-         console.log("Everything should be done now!");
     });
-    console.log("Goodbye!");
   });
+
 
