d570caf59618dcdc68451f0623987d513d7a01fc
OHutchyHutch <ohutchins@comcast.net>
2020-06-13 02:29:46 -0400

    Website V 0.100
    Fixed last update breaking everything, i think. I can't really test it. I'm relying on github's servers

--- js/accordion.js
+++ js/accordion.js
@@ -2,7 +2,20 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("touchstart click", function() {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("touchstart", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
