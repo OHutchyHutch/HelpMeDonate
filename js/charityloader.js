1b17f710eb725da003cdfe82ceb0cc73d079f95d
OHutchyHutch <ohutchins@comcast.net>
2020-06-13 02:50:13 -0400

    Website V 0.103

--- js/charityloader.js
+++ js/charityloader.js
@@ -1,13 +1,10 @@
 $(function() {
     $.getJSON('charities/charities.json', function(data) {
       //currentevents
-      console.log("Webpage loaded");
       $.each(data.currentevents, function(key, val) {
-        console.log("Populated CurrentEvents");
         populateCharities(val, "#CurrentEventsCharities")
        })
        $.each(data.environmental, function(key, val) {
-        console.log("Populated CurrentEvents");
         populateCharities(val, "#EnviromentalCharities")
        })
 
@@ -15,10 +12,8 @@ $(function() {
 });
 
 function populateCharities(val, location){
-  console.log("Starting populateCharities()");
   charityName = val.charityName; 
   charityNameSpaces = charityName.replaceAll("-"," ");
-  console.log("charityNameSpaces created successfully");
   $( location ).append('<a id="'+val.charityName+'LINK" href='+val.charityLink+' style="font-size:inherit;font-weight: normal;"></a>');
   $( '#'+val.charityName+'LINK' ).append( '<div id="'+val.charityName+'"style="width:40%; display:inline-flex; margin:4.5%;"></div>');
   $( '#'+val.charityName ).append( '<div class="card mb-3 my-2 cardoverall" style="max-width: 100%; max-height: 90%;" id="'+ val.charityName +'CARD"></div>');
